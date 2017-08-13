3a7f74a6c62720948ff0db0fe7451bcc32835e10
Cavidan <acavidan.p102@code.edu.az>
2017-08-13 19:01:08 +0400

    first commit

---
+++ yilan/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yilan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        yilan yilanimiz = new yilan();
+        istiqamet istiqametimiz;
+        PictureBox[] pb_yilanparcalari;
+        bool yem_varmi = false;
+        Random rastGele = new Random();
+        PictureBox pb_yem;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pb_yilanparcalari = new PictureBox[0];
+            for(int i = 0; i < 3; i++)
+            {
+                Array.Resize(ref pb_yilanparcalari, pb_yilanparcalari.Length + 1);
+                pb_yilanparcalari[i] = Pb_ekle();
+            }
+            timer1.Start();
+        }
+        
+        private PictureBox Pb_ekle()
+        {
+            
+            PictureBox pb = new PictureBox();
+            pb.Size = new Size(10, 10);
+            pb.BackColor = Color.White;
+            pb.Location = yilanimiz.GetPos(pb_yilanparcalari.Length - 1);
+            panel1.Controls.Add(pb);
+            return pb;
+        }
+
+        private void Pb_guncelle()
+        {
+            for(int i = 0; i < pb_yilanparcalari.Length; i++)
+            {
+                pb_yilanparcalari[i].Location = yilanimiz.GetPos(i);
+            }
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode==Keys.Up || e.KeyCode == Keys.W)
+            {
+                istiqametimiz = new istiqamet(0, -10);
+
+            }else if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+            {
+                istiqametimiz = new istiqamet(0, 10);
+
+            }else if(e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                istiqametimiz = new istiqamet(-10, 0);
+
+            }else if(e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+            {
+                istiqametimiz = new istiqamet(10, -0);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            yilanimiz.hereketEt(istiqametimiz);
+            Pb_guncelle();
+            Yem_Olustur();
+            yemi_yedikmi();
+        }
+        public void Yem_Olustur()
+        {
+            if (!yem_varmi) { 
+            PictureBox pb = new PictureBox();
+            pb.BackColor = Color.Red;
+            pb.Size = new Size(10, 10);
+            pb.Location = new Point(rastGele.Next(panel1.Width / 10) * 10, rastGele.Next(panel1.Height / 10) * 10);
+            pb_yem = pb;
+            yem_varmi = true;
+            panel1.Controls.Add(pb);
+        }
+        }
+        public void yemi_yedikmi()
+        {
+            if (yilanimiz.GetPos(0) == pb_yem.Location)
+            {
+                yilanimiz.boyu();
+                Array.Resize(ref pb_yilanparcalari, pb_yilanparcalari.Length + 1);
+                pb_yilanparcalari[pb_yilanparcalari.Length - 1] = Pb_ekle();
+                yem_varmi = false;
+                panel1.Controls.Remove(pb_yem);
+
+            }
+        }
+    }
+}
---
+++ yilan/yilan.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace yilan
+{
+    class yilan
+    {
+        yilan_Parcalari[] yilan_parca;
+        int yilan_buyuklugu;
+        istiqamet istiqametimiz;
+
+        public yilan()
+        {
+            yilan_parca = new yilan_Parcalari[3];
+            yilan_buyuklugu = 3;
+            yilan_parca[0] = new yilan_Parcalari(150, 150);
+            yilan_parca[1] = new yilan_Parcalari(150, 160);
+            yilan_parca[2] = new yilan_Parcalari(170, 150);
+
+        }
+        public void hereketEt(istiqamet istiqamet)
+
+        {
+            istiqametimiz = istiqamet;
+            if(istiqamet != null)
+            {
+                for(int i = yilan_parca.Length - 1; i < 0; i--)
+                {
+                    yilan_parca[i] = new yilan_Parcalari(yilan_parca[i - 1].x_, yilan_parca[i - 1].y_);
+                }
+                yilan_parca[0] = new yilan_Parcalari(yilan_parca[0].x_ + istiqamet._x, yilan_parca[0].y_ + istiqamet._y);
+            }
+
+        }
+        public void boyu()
+        {
+            Array.Resize(ref yilan_parca, yilan_parca.Length + 1);
+            yilan_parca[yilan_parca.Length-1]=new yilan_Parcalari(yilan_parca[yilan_parca.Length-2].x_-istiqametimiz._x, yilan_parca[yilan_parca.Length - 2].y_-istiqametimiz._y);
+            yilan_buyuklugu++;
+
+        }
+        public Point GetPos(int number)
+        {
+            return new Point(yilan_parca[number].x_, yilan_parca[number].y_);
+        }
+    }
+    class yilan_Parcalari
+    {
+        public int x_;
+        public int y_;
+        public readonly int size_x;
+        public readonly int size_y;
+        public yilan_Parcalari(int x, int y)
+        {
+            x_ = x;
+            y_ = y;
+            size_x = 10;
+            size_y = 10;
+
+        }
+    }
+    class istiqamet
+    {
+        public readonly int _x;
+        public readonly int _y;
+        public istiqamet(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+    }
+}
